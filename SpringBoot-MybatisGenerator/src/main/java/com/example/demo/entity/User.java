2d009e42b2e51208d609cdc773bb6408f9de1893
Rumoers <1347903339@qq.com>
2020-01-14 22:51:47 +0800

    SpringBoot使用Mybatis generator生成文件

---
+++ SpringBoot-MybatisGenerator/src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,57 @@
+package com.example.demo.entity;
+
+public class User {
+    private Integer id;
+
+    private String name;
+
+    private Integer age;
+
+    private Double money;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Double getMoney() {
+        return money;
+    }
+
+    public void setMoney(Double money) {
+        this.money = money;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", name=").append(name);
+        sb.append(", age=").append(age);
+        sb.append(", money=").append(money);
+        sb.append("]");
+        return sb.toString();
+    }
+}
